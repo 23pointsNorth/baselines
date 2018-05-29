5c28fd7f883e3b09bef3dc96900438a367e3d414
Simon Smith <artificialsimon@gmail.com>
2018-05-29 19:33:32 +0100

    created state based on features

--- baselines/qlearning/main.py
+++ baselines/qlearning/main.py
@@ -22,84 +22,10 @@ import numpy as np
 from baselines.qlearning import qlearning
 from qlearning import build_state
 import pickle
-#from functools import reduce
-
-#def run(env_id, seed, noise_type, layer_norm, evaluation, custom_log_dir, **kwargs):
-    ## Configure things.
-    #rank = MPI.COMM_WORLD.Get_rank()
-    #if rank != 0:
-        #logger.set_level(logger.INFO)
-
-    #train_recording_path = os.path.join(custom_log_dir, env_id, 'train', datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-    #os.makedirs(train_recording_path)
-    
-    ## Create envs.
-    #env = gym.make(env_id)
-    #env = TraceRecordingWrapper(env, directory=train_recording_path, buffer_batch_size=10)
-    #logger.info('TraceRecordingWrapper dir: {}'.format(env.directory))
-    ## env = bench.Monitor(env, os.path.join(train_recording_path, 'log'))
-
-    #if evaluation and rank==0:
-        #eval_recording_path = os.path.join(custom_log_dir, env_id, 'eval', datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
-        #os.makedirs(eval_recording_path)
-        
-        #eval_env = gym.make(env_id)
-        #eval_env = TraceRecordingWrapper(eval_env, directory=eval_recording_path, buffer_batch_size=10)
-        #logger.info('TraceRecordingWrapper eval dir: {}'.format(eval_env.directory))
-        ## eval_env = bench.Monitor(eval_env, os.path.join(logger.get_dir(), 'gym_eval'))
-        ## env = bench.Monitor(env, None)
-    #else:
-        #eval_env = None
-
-    ## Parse noise_type
-    #action_noise = None
-    #param_noise = None
-    #nb_actions = env.action_space.shape[-1]
-    #for current_noise_type in noise_type.split(','):
-        #current_noise_type = current_noise_type.strip()
-        #if current_noise_type == 'none':
-            #pass
-        #elif 'adaptive-param' in current_noise_type:
-            #_, stddev = current_noise_type.split('_')
-            #param_noise = AdaptiveParamNoiseSpec(initial_stddev=float(stddev), desired_action_stddev=float(stddev))
-        #elif 'normal' in current_noise_type:
-            #_, stddev = current_noise_type.split('_')
-            #action_noise = NormalActionNoise(mu=np.zeros(nb_actions), sigma=float(stddev) * np.ones(nb_actions))
-        #elif 'ou' in current_noise_type:
-            #_, stddev = current_noise_type.split('_')
-            #action_noise = OrnsteinUhlenbeckActionNoise(mu=np.zeros(nb_actions), sigma=float(stddev) * np.ones(nb_actions))
-        #else:
-            #raise RuntimeError('unknown noise type "{}"'.format(current_noise_type))
-
-    ## Configure components.
-    #memory = Memory(limit=int(1e6), action_shape=env.action_space.shape, observation_shape=env.observation_space.shape)
-    #critic = Critic(layer_norm=layer_norm)
-    #actor = Actor(nb_actions, layer_norm=layer_norm)
-
-    ## Seed everything to make things reproducible.
-    #seed = seed + 1000000 * rank
-    #logger.info('DDPG: rank {}: seed={}, logdir={}'.format(rank, seed, logger.get_dir()))
-    #tf.reset_default_graph()
-    #set_global_seeds(seed)
-    #env.seed(seed)
-    #if eval_env is not None:
-        #eval_env.seed(seed)
-
-    ## Disable logging for rank != 0 to avoid noise.
-    #if rank == 0:
-        #start_time = time.time()
-    #training.train(env=env, eval_env=eval_env, param_noise=param_noise,
-        #action_noise=action_noise, actor=actor, critic=critic, memory=memory, **kwargs)
-    #env.close()
-    #if eval_env is not None:
-        #eval_env.close()
-    #if rank == 0:
-        #logger.info('total runtime: {}s'.format(time.time() - start_time))
 
 
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
     parser.add_argument('--env-id', type=str, default='apl-nav-godiland-v0')
     boolean_flag(parser, 'render-eval', default=False)
     boolean_flag(parser, 'layer-norm', default=True)
@@ -155,25 +81,11 @@ if __name__ == '__main__':
     logger.set_level(logger.INFO)
     #env = gym.wrappers.Monitor(env, '/tmp/cartpole-experiment-1', force=True)
         # video_callable=lambda count: count % 10 == 0)
-
     #goal_average_steps = 255
     max_number_of_steps = 10000
     number_of_episodes = 1000
     last_time_steps_reward = np.ndarray(0)
-
-    #n_bins = 8
-    #n_bins_angle = 10
-
     number_of_features = env.observation_space.n
-    #last_time_steps = np.ndarray(0)
-
-    # Number of states is huge so in order to simplify the situation
-    # we discretize the space to: 10 ** number_of_features
-    #cart_position_bins = pandas.cut([-2.4, 2.4], bins=n_bins, retbins=True)[1][1:-1]
-    #pole_angle_bins = pandas.cut([-2, 2], bins=n_bins_angle, retbins=True)[1][1:-1]
-    #cart_velocity_bins = pandas.cut([-1, 1], bins=n_bins, retbins=True)[1][1:-1]
-    #angle_rate_bins = pandas.cut([-3.5, 3.5], bins=n_bins_angle, retbins=True)[1][1:-1]
-
     # File to store que Q function
     timestr = time.strftime("%Y%m%d-%H%M%S")
     fileName = "./q_functions/" + timestr + ".qf"
@@ -194,11 +106,6 @@ if __name__ == '__main__':
         state = build_state(observation)
         print("Episode: {}/{}".format(i_episode, number_of_episodes))
 
-        #cart_position, pole_angle, cart_velocity, angle_rate_of_change = observation
-        #state = build_state([to_bin(cart_position, cart_position_bins),
-                         #to_bin(pole_angle, pole_angle_bins),
-                         #to_bin(cart_velocity, cart_velocity_bins),
-                         #to_bin(angle_rate_of_change, angle_rate_bins)])
         if args['learning']:
           os.makedirs(os.path.dirname(fileName), exist_ok=True)
           with open(fileName, "wb") as fp:   #Pickling
